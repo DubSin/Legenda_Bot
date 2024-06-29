ece907fe3ba0b410a447d1a539e03d624a10476e
Chunga changa <mondeblum@gmail.com>
2024-06-29 21:52:09 +0300

    1.0 product version

---
+++ db.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+
+class BoT_DB:
+    def __init__(self, db):
+        self.db = sqlite3.connect(db)
+        self.cursor = self.db.cursor()
+
+    def get_yuan(self):
+        result = self.cursor.execute("SELECT yuan FROM `exchange_rates`")
+        return result.fetchone()
+
+    def get_dollar(self):
+        result = self.cursor.execute("SELECT dollar FROM `exchange_rates`")
+        return result.fetchone()
+
+    def get_euro(self):
+        result = self.cursor.execute("SELECT euro FROM `exchange_rates`")
+        return result.fetchone()
+
+    def update_yuan(self, new_cur):
+        self.cursor.execute("UPDATE `exchange_rates` SET `yuan` = ? WHERE `id` = 1", (new_cur,))
+        return self.db.commit()
+
+    def update_dollar(self, new_cur):
+        self.cursor.execute("UPDATE `exchange_rates` SET `dollar` = ? WHERE `id` = 1", (new_cur,))
+        return self.db.commit()
+
+    def update_euro(self, new_cur):
+        self.cursor.execute("UPDATE `exchange_rates` SET `euro` = ? WHERE `id` = 1", (new_cur,))
+        return self.db.commit()
